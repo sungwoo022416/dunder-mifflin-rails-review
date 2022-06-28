af7b085746cdb42c4a5689fff6a6a1148b8ba7cb
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-06-28 12:26:14 -0700

    commit tree 61419492cc9e554448281a1a9ff328ecc915c214

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,16 @@
 class DogsController < ApplicationController
+    before_action :set_dog, only: [:show, :edit, :update, :destory]
+
+    def index
+        @dogs = Dog.all
+    end
+
+    private 
+    def set_dog
+        @dog = Dog.find(params[:id])
+    end
+
+    def dog_params
+        params.require(:dog).permit!
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,14 @@
 class EmployeesController < ApplicationController
+    before_action :set_employee, only: [:show, :edit, :update, :destory]
+
+
+
+    private 
+    def set_employee
+        @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+        params.require(:employee).permit!
+    end
 end
